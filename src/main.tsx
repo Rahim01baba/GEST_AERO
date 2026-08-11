365fd7cad59f908457c15c3ccc70034fb2623c1a
Rahim01baba <babasangare4@gmail.com>
2026-08-11 09:51:49 +0000

    Add recovery type redirect to reset-password
    Redirect to reset-password if recovery type is in hash.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+if (window.location.hash.includes('type=recovery') && window.location.pathname !== '/reset-password') {
+  window.history.replaceState(null, '', '/reset-password' + window.location.hash)
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
